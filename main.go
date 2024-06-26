140659979f61a334e336f7e92e5dd1389c47c2fa
Anderson Ribeiro <1010263+ansssu@users.noreply.github.com>
2024-06-26 19:00:34 +0100

    implement bearer token authorization

--- main.go
+++ main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
+var tokens []string
+
 func main() {
 	r := gin.Default()
-	r.GET("/resource", gin.BasicAuth(gin.Accounts{
+
+	r.POST("/login", gin.BasicAuth(gin.Accounts{
 		"admin": "secret",
-	}), func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"data": "resource data",
+	}), func(c *gin.Context) {
+		token, _ := randomHex(20)
+		tokens = append(tokens, token)
+		c.JSON(http.StatusOK, gin.H{
+			"token": token,
 		})
 	})
+
+	r.GET("/resource", func(c *gin.Context) {
+		bearerToken := c.Request.Header.Get("Authorization")
+		reqToken := strings.Split(bearerToken, " ")[1]
+		for _, token := range tokens {
+			if token == reqToken {
+				c.JSON(http.StatusOK, gin.H{
+					"data": "resource data",
+				})
+			}
+		}
+	})
 	r.Run()
+}
+
+func randomHex(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
 }
